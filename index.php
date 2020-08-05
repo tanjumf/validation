a67fbd00c5cc1c3ffe1f60f4b81f287eb6c1a75b
tanjum <69140369+tanjumf@users.noreply.github.com>
2020-08-05 13:23:13 +0600

    2nd commit

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-include_once "session.php";
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +6,22 @@ include_once "session.php";
     <title>Document</title>
 </head>
 <body>
+   <?php
+    session_start();
+    if(isset($_SESSION["uname"])){
+        if((time() - $_SESSION['last_login_timestamp'])>60){
+            header("location:logout.php");
+        }
+        else{
+            $_SESSION['last_login_timestap'] = time();
+            echo "<p align='center'><a href='logout.php'> Logout</a></p>";
+        }
+     
+    }
+            else{
+            header('location:logout.php');
+            }
+    ?>
     <h1>Weclome</h1>
 </body>
 </html>
--- validation.php
+++ validation.php
@@ -1,5 +1,11 @@
 <?php 
 
+ session_start();
+
+
+if(isset($_POST["submit"])){
+     $_SESSION["uname"] = $_POST["uname"];
+     $_SESSION['last_login_timestamp'] = time();
 
 $conn = mysqli_connect('localhost','root','','userregistration');
 
@@ -11,9 +17,14 @@ $conn = mysqli_connect('localhost','root','','userregistration');
 
 $sql ="select * from usertable where uname = '$uname' AND password = '$password'  "; 
 $result = mysqli_query($conn, $sql);
+ 
+     
+
+
+
 if(mysqli_num_rows($result)>0)
 {
-    session_start();
+   
     $_SESSION['uname'] = $uname;
     header("Location: index.php");
 }
@@ -21,5 +32,5 @@ else
 {
 	echo "User not found";
 }
-
+    }
 ?>
